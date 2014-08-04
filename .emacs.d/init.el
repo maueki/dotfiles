2726e023f07590af5b9e8a2412a71e2df20c272b
Ueki Masaru <masaru.ueki@itage.co.jp>
2014-08-04 10:23:20 +0900

    helm 表示優先度の変更

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -36,10 +36,10 @@
 (global-set-key (kbd "C-x I") 'helm-imenu)
 
 (setq helm-for-files-preferred-list
-      '(helm-source-buffers-list
-        helm-source-bookmarks
-        helm-source-recentf
+      '(helm-source-bookmarks
+	helm-source-buffers-list
 	helm-source-ls-git
+        helm-source-recentf
         helm-source-file-cache
         ;; helm-source-files-in-current-dir
         helm-source-locate))
